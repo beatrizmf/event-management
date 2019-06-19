0d93f57a925706b94fedc07d166c72315403592c
beatrizf13 <azz.felix7@gmail.com>
2019-06-19 16:30:09 -0300

    events administrator

--- src/pages/Events/ListEventsAdministrator/index.js
+++ src/pages/Events/ListEventsAdministrator/index.js
@@ -6,138 +6,65 @@ import dateFns from 'date-fns';
 
 
 class ListEventsAdministrator extends Component {
-	state = {
-		events: [],
-	}
+  state = {
+    events: [],
+  }
 
-	componentDidMount() {
-		this.handleListEventsAdministrator();
-	}
+  componentDidMount() {
+    this.handleListEventsAdministrator();
+  }
 
-	handleListEventsAdministrator = async () => {
-		try {
-			const response = await api.get('/admin/events')
-			this.setState({ events: response.data })
+  handleListEventsAdministrator = async () => {
+    try {
+      const response = await api.get('/admin/events')
+      this.setState({ events: response.data })
 
-		} catch (err) {
-			console.log(err);
-		}
-	}
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-	render() {
-		return (
-			<Main>
-				{
-					this.state.events.map(event => (
-						<>
-							<div className="card border-left-primary shadow">
-								<div className="card-body">
-									<div className="no-gutters align-items-center">
-										<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">{dateFns.format(event.startsIn, 'MM/DD/YYYY HH:mm')}</div>
-										<div className="h5 mb-0 font-weight-bold text-gray-800">{event.name}</div>
-										<div className="row">
-											<div className="mt-2 mx-2">
-												<button className="btn btn-primary btn-icon-split btn-block" data-toggle="modal" data-target="#enrolledsList">
-													<span className="icon text-white-50">
-														<i className="fas fa-check"></i>
-													</span>
-													<span className="text">Enrolled</span>
-												</button>
-											</div>
-											<div className="mt-2 mx-2">
-												<button className="btn btn-success btn-icon-split btn-block" data-toggle="modal">
-													<span className="icon text-white-50">
-														<i className="fas fa-info-circle"></i>
-													</span>
-													<Link className="text-decoration-none text text-white" to={`/admin/events/frequency/${event._id}`}>Attendance</Link>
-												</button>
-											</div>
-											<div className="mt-2 mx-2">
-												<button className="btn btn-danger btn-icon-split btn-block" data-toggle="modal" data-target="#modalDelete">
-													<span className="icon text-white-50">
-														<i className="fas fa-trash"></i>
-													</span>
-													<span className="text">Delete</span>
-												</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="modal fade" id="modalDelete" tabIndex="-1" role="dialog" aria-labelledby="eventsDetails" aria-hidden="true">
-								<div className="modal-dialog modal-dialog-centered" role="document">
-									<div className="modal-content">
-										<div className="modal-header">
-											<h5 className="modal-title" id="exampleModalLongTitle">Delete Event</h5>
-											<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div className="modal-body">
-											Do you really want to delete the event?
-										</div>
-										<div className="modal-footer">
-											<button type="button" className="btn btn-success" data-dismiss="modal">Confirm</button>
-											<button type="button" className="btn btn-danger" data-dismiss="modal">Cancel</button>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="modal fade bd-example-modal-xl" id="enrolledsList">
-								<div className="modal-dialog modal-dialog-centered modal-xl" role="document">
-									<div className="modal-content">
-										<div className="modal-header">
-											<h5 className="modal-title" id="exampleModalLongTitle">Enrolleds List</h5>
-											<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div className="modal-body">
-											<table className="table table-bordered dataTable" id="dataTable" width="100%" cellSpacing="0" role="grid" aria-describedby="dataTable_info">
-												<thead >
-													<tr role="row" >
-														<th className="sorting_asc" tabIndex="0" aria-controls="dataTable" rowSpan="1" colSpan="1">Name</th>
-														<th className="sorting" tabIndex="0" aria-controls="dataTable" rowSpan="1" colSpan="1">E-mail</th>
-														<th className="sorting" tabIndex="0" aria-controls="dataTable" rowSpan="1" colSpan="1">Status</th>
-													</tr>
-												</thead>
-												<tfoot>
-													<tr>
-														<th rowSpan="1" colSpan="1">Name</th>
-														<th rowSpan="1" colSpan="1">E-mail</th>
-														<th rowSpan="1" colSpan="1">Status</th>
-													</tr>
-												</tfoot>
-												<tbody>
-													{
-														this.state.events.map(event => (
-															event.enrolleds.map(enrolled => (
-																<tr role="row" className="odd">
-																	<td className="sorting_1">{enrolled}</td>
-																	<td>{enrolled}</td>
-																	<td>{enrolled}</td>
-																</tr>
-															))
-														))
-													}
-												</tbody>
-											</table>
-										</div>
-										<div className="modal-footer">
-											<button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
-										</div>
-									</div>
-								</div>
-							</div>
-							<br />
-						</>
-					))
-				}
-			</Main>
-		);
-	}
+  render() {
+    return (
+      <Main>
+        <div className="row">
+          {
+            this.state.events.map(event => (
+              <div className="col-lg-6 card border-left-primary shadow">
+                <div className="card-body">
+                  <div className="no-gutters align-items-center">
+                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Starts in: {dateFns.format(event.startsIn, 'MM/DD/YYYY HH:mm')}</div>
+                    <div className="h5 mb-0 font-weight-bold text-gray-800">{event.name}</div>
+                    <p>{event.description}</p>
+                  </div>
+                  <div className="row">
+                    <button className="btn btn-secondary mr-1">
+                      <span className="text">Enrolleds</span>
+                    </button>
+                    <button className="btn btn-secondary mr-1">
+                      <span className="text">Confirmed Enrolleds</span>
+                    </button>
+                    <button className="btn btn-secondary mr-1">
+                      <span className="text">Details</span>
+                    </button>
+                    <button className="btn btn-secondary mr-1">
+                      <span className="text">Edit</span>
+                    </button>
+                    <button className="btn btn-danger mr-1">
+                      <span className="text">Delete</span>
+                    </button>
+                    <button className="btn btn-success">
+                      <Link className="text-decoration-none text text-white" to={`/admin/events/frequency/${event._id}`}>Attendance</Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </Main>
+    );
+  }
 }
 
 export default ListEventsAdministrator;
